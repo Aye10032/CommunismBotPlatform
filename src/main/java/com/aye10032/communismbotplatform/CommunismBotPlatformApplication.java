e9d7347ea112fdf55fb3423e5e1430b836babc0b
Aye10032 <aye10032@gmail.com>
2023-08-09 15:07:20 +0800

    update

--- src/main/java/com/aye10032/communismbotplatform/CommunismBotPlatformApplication.java
+++ src/main/java/com/aye10032/communismbotplatform/CommunismBotPlatformApplication.java
@@ -1,9 +1,16 @@
 package com.aye10032.communismbotplatform;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.aye10032"})
+@MapperScans({
+        @MapperScan("com.aye10032.mapper")
+})
+@EnableTransactionManagement
 public class CommunismBotPlatformApplication {
 
     public static void main(String[] args) {
