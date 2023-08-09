3671474898bf3e5e83513de98dfde203f3c9e402
Aye10032 <aye10032@gmail.com>
2023-08-09 10:50:02 +0800

    init spring

---
+++ src/main/java/com/aye10032/communismbotplatform/CommunismBotPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aye10032.communismbotplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunismBotPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunismBotPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aye10032/communismbotplatform/CommunismBotPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aye10032.communismbotplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunismBotPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
